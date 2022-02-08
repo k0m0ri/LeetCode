d01944d27c776877ad22db5e4b0e7e6aff3cd3d3
H_Komori <komori.sc14@gmail.com>
2022-02-08 21:39:37 +0900

    コーナーケースの処理を追加

--- 83/remove_duplicates_from_sorted_list.py
+++ 83/remove_duplicates_from_sorted_list.py
@@ -5,10 +5,16 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def deleteDuplicates(self, head):
-        curr = head
 
+        # cornercase: if head or head.next == None, return
+        if head is None or head.next is None:
+            return head
+
+        curr = head
         while curr.next is not None:
             if curr.val == curr.next.val:
                 curr.next = curr.next.next
