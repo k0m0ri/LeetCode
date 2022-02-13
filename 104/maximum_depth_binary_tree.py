11c3b3bd2a5f7cb3f4f0fb5d3bf46e1bb0e79238
H_Komori <komori.sc14@gmail.com>
2022-02-13 15:15:14 +0900

    コードを追加
    二分木の深さを求める

---
+++ 104/maximum_depth_binary_tree.py
@@ -0,0 +1,20 @@
+import math
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+        
+        max_depth_left = self.maxDepth(root.left)
+
+        max_depth_right = self.maxDepth(root.right)
+
+        return max(max_depth_left, max_depth_right) + 1
+        
